8682a2ccf05815e16e8eedae764789f8c9792f92
naolson2025 <naolson.1819@gmail.com>
2019-10-21 18:01:03 -0500

    added drink api

--- Project4/party/urls.py
+++ Project4/party/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.weather_list, name='weather_list')
+    path('', views.weather_list, name='weather_list'),
+    path('', views.drink_list, name='drink_list')
 ]
--- Project4/party/views.py
+++ Project4/party/views.py
@@ -17,7 +17,8 @@ def weather_list(request):
     day_3 = []
     day_4 = []
     day_5 = []
-
+    print(request)
+    print(request.GET)
     if 'city' and 'country_code' in request.GET:
         city = request.GET['city']
         country_code = request.GET['country_code']
@@ -31,6 +32,33 @@ def weather_list(request):
 
         # call the sort data method from weatherAPI.py in order to format the data
         day_1, day_2, day_3, day_4, day_5 = sort_data(weather_results)
- 
+        return render(request, 'party_templates/home.html', {'day_1': day_1, 'day_2': day_2, 'day_3': day_3, 'day_4': day_4, 'day_5': day_5})
+    elif 'random-drink' in request.GET:
+        random_drink = drink_list(request)
+        return render(request, 'party_templates/home.html', {'random_drink': random_drink}) 
     #return render(request, 'party_templates/home.html', {'weather_results': weather_results}, {'day_1': day_1})
-    return render(request, 'party_templates/home.html', {'day_1': day_1, 'day_2': day_2, 'day_3': day_3, 'day_4': day_4, 'day_5': day_5})
+
+
+
+def drink_list(request):
+    random_drink = {}
+    print('made it here')
+    if 'random-drink' in request.GET:
+    #if request.method == 'GET':
+
+        url = "https://the-cocktail-db.p.rapidapi.com/random.php"
+
+        headers = {
+        'x-rapidapi-host': "the-cocktail-db.p.rapidapi.com",
+        'x-rapidapi-key': "611386d4a7mshdfb5beebe926ae1p127f3ejsn1014e979f01a"
+        }
+
+        #query = {'x-rapidapi-host': "the-cocktail-db.p.rapidapi.com", 'x-rapidapi-key': "611386d4a7mshdfb5beebe926ae1p127f3ejsn1014e979f01a"}
+        random_drink = requests.request("GET", url, headers=headers).json()
+        #random_drink = requests.get(url, params=query)
+        print(random_drink)
+
+    print()
+    print(random_drink)
+    #return render(request, 'party_templates/home.html', {'random_drink': random_drink})    
+    return random_drink
