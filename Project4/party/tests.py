1017d4732944106d701d90513087ba5a9fd49677
joshkallagunta <nq6647xv@go.minneapolis.edu>
2019-10-30 00:52:36 -0500

    added one test for testing branch

--- Project4/party/tests.py
+++ Project4/party/tests.py
@@ -1,10 +1,27 @@
 from django.test import TestCase
+import unittest
+from unittest.mock import patch
+import drinkAPI
+import requests
+
 
 # Create your tests here.
 
 class TestDrinkAPI(TestCase):
 
     test_drink_db_url = 'test_drink.db'
+
+    #@patch ('drinkAPI.call_drink_api')
+    def test_drink_api_call(self):
+        responce = requests.get('https://the-cocktail-db.p.rapidapi.com/random.php')
+
+        self.assertTrue(responce.ok)
+
+
+
+        
+
+
 
 
 class TestFoodAPI(TestCase):
@@ -14,6 +31,7 @@ class TestFoodAPI(TestCase):
 class TestMediaAPI(TestCase):
 
     test_media_db_url = 'test_media.db'
+
 
 
 
