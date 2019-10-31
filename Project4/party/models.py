d4b735a16ffafae423fbee353914c02dfc6984b1
l;kj <l;kj>
2019-10-31 16:27:22 -0500

    Removed ListCharField instances as this is mysql

--- Project4/party/models.py
+++ Project4/party/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django_mysql.models import ListCharField
 from django.db.models import CharField, FloatField, DateTimeField, Model
 
 # Create your models here.
@@ -7,12 +6,12 @@ from django.db.models import CharField, FloatField, DateTimeField, Model
 # Create a model to make a table in the database for the Weather information
 class Weather(Model):
     # column for the date and time
-    # date = models.DateTimeField()
-    date = ListCharField(base_field=CharField(max_length=100), max_length=(10000))
+    date = models.DateTimeField()
+    #date = ListCharField(base_field=CharField(max_length=100), max_length=(10000))
     # Found the set null on stack overflow
     # column for the temperature in Farinhight
-    # temp = models.FloatField(null=True, blank=True, default=None)
-    temp = ListCharField(base_field=CharField(max_length=100), max_length=(10000))
+    temp = models.FloatField(null=True, blank=True, default=None)
+    #temp = ListCharField(base_field=CharField(max_length=100), max_length=(10000))
 
     def __str__(self):
         return f'{self.date}, {self.temp}'
@@ -31,7 +30,8 @@ class FoodRecipe(Model):
 
     url = models.CharField(max_length=200)
     # Found ListCharField in docs https://django-mysql.readthedocs.io/en/latest/model_fields/list_fields.html
-    food_ingredients = ListCharField(base_field=CharField(max_length=100), max_length=(10000))
+    #food_ingredients = ListCharField(base_field=CharField(max_length=100), max_length=(10000))
+    food_ingredients = CharField(max_length=200)
 
     def __str__(self):
         return f'{self.recipe_name}, {self.url}, {self.food_ingredients}'
@@ -40,9 +40,11 @@ class FoodRecipe(Model):
 class DrinkRecipe(Model):
     drink_recipe_name = models.CharField(max_length=200)
 
-    drink_ingredients = ListCharField(base_field=CharField(max_length=100), max_length=(10000))
+    #drink_ingredients = ListCharField(base_field=CharField(max_length=100), max_length=(10000))
+    drink_ingredients = CharField(max_length=200)
 
-    drink_measurements = ListCharField(base_field=CharField(max_length=100), max_length=(10000))
+    #drink_measurements = ListCharField(base_field=CharField(max_length=100), max_length=(10000))
+    drink_measurements = CharField(max_length=200)
 
     def __str__(self):
         return f'{self.drink_recipe_name}, {self.drink_ingredients}, {self.drink_measurements}'
